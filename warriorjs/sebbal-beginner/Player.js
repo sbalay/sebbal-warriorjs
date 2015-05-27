3dd0adc367ba44e5102c999a76fba9f349ee53fe
Sebastian Balay <snbalay@gmail.com>
2015-05-27 18:44:01 -0300

    Solving 4th level

--- warriorjs/sebbal-beginner/Player.js
+++ warriorjs/sebbal-beginner/Player.js
@@ -1,7 +1,10 @@
 class Player {
   playTurn(warrior) {
+    if (!this.initialized) {
+      this.initialize(warrior);
+    }
     if (warrior.feel().isEmpty()) {
-      if (warrior.health() < 12) {
+      if (warrior.health() < 13 && !this.isUnderAttack(warrior)) {
         warrior.rest();
       } else {
         warrior.walk();
@@ -9,6 +12,20 @@ class Player {
     } else {
       warrior.attack();
     }
+    this.finishTurn(warrior);
+  }
+
+  initialize(warrior) {
+    this.initialized = true;
+    this.health = warrior.health();
+  }
+
+  finishTurn(warrior) {
+    this.health = warrior.health();
+  }
+
+  isUnderAttack(warrior) {
+    return warrior.health() < this.health;
   }
 }
 
